9070a9c45ebece457aec3935b7fc4299299d5a33
BaoTran <trangiabao1203@gmail.com>
2017-07-07 15:51:39 +0700

    Add Jar Estimate
    Signed-off-by: BaoTran <trangiabao1203@gmail.com>

---
+++ mobile/src/main/java/com/trangiabao/sixjars/adapter/JarEstimateAdapter.kt
@@ -0,0 +1,57 @@
+package com.trangiabao.sixjars.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.trangiabao.sixjars.R
+import com.trangiabao.sixjars.model.Jar
+import kotlinx.android.synthetic.main.item_jar_estimate.view.*
+import java.math.BigDecimal
+import java.text.DecimalFormat
+
+class JarEstimateAdapter(private var salary: Double, private var year: Int = 1) : RecyclerView.Adapter<JarEstimateAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private var lists: MutableList<Jar> = mutableListOf()
+    private val df = DecimalFormat("###,###,###,###,###.##")
+
+    var List: MutableList<Jar>
+        get() = this.lists
+        set(value) {
+            this.lists = value
+            notifyDataSetChanged()
+        }
+
+    fun updateYear(year: Int) {
+        this.year = year
+        notifyDataSetChanged()
+    }
+
+    fun updateSalary(income: Double) {
+        this.salary = income
+        notifyDataSetChanged()
+    }
+
+    fun getTotal(): String {
+        return df.format(salary * year * 12)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.item_jar_estimate, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val model: Jar = lists[position]
+        holder!!.itemView.run {
+            val amount: Double = salary * model.percent / 100 * year * 12
+            txtAmount.text = df.format(BigDecimal(amount))
+            txtJar.text = model.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+}
--- mobile/src/main/java/com/trangiabao/sixjars/fragment/EstimateFragment.kt
+++ mobile/src/main/java/com/trangiabao/sixjars/fragment/EstimateFragment.kt
@@ -1,19 +1,66 @@
 package com.trangiabao.sixjars.fragment
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.trangiabao.sixjars.R
+import com.trangiabao.sixjars.adapter.JarEstimateAdapter
+import com.trangiabao.sixjars.model.Jar
+import com.trangiabao.sixjars.presenter.JarPresenter
+import com.trangiabao.sixjars.system.BaseFragment
+import com.trangiabao.sixjars.ui.NumericEditText
+import com.trangiabao.sixjars.view.JarView
+import kotlinx.android.synthetic.main.fragment_estimate.view.*
 
-class EstimateFragment : Fragment() {
+class EstimateFragment : BaseFragment(), JarView {
+
+    private var adapter: JarEstimateAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.fragment_estimate, container, false)
-
+        addControls(view)
+        addEvents(view)
         return view
     }
 
+    private fun addControls(view: View) {
+        view.run {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            adapter = JarEstimateAdapter(0.0)
+            recyclerView.adapter = adapter
+            spinner.setItems((1..20).toMutableList())
+        }
+    }
+
+    private fun addEvents(view: View) {
+        val presenter: JarPresenter = JarPresenter(context, this)
+        presenter.getAll()
+        view.run {
+            txtSalary.addNumericValueChangedListener(object : NumericEditText.NumericValueWatcher {
+                override fun onChanged(newValue: Double) {
+                    adapter!!.updateSalary(newValue)
+                    txtTotal.text = adapter!!.getTotal()
+                }
+
+                override fun onCleared() {
+                }
+            })
+
+            spinner.setOnItemSelectedListener { _, position, _, _ ->
+                adapter!!.updateYear(position + 1)
+                txtTotal.text = adapter!!.getTotal()
+            }
+        }
+    }
+
+    override fun onGetListResult(list: MutableList<Jar>) {
+        adapter!!.List = list
+    }
+
+    override fun onUpdateResult(result: Boolean) {
+        // TODO("Do nothing")
+    }
 }
